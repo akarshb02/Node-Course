30baa5b5ad3036c3e5685dd89c53fdede5454fac
Akarsh <akarshb02@gmail.com>
2021-01-05 20:01:22 +0530

    Delete mongoose.js

--- Rest-Api/src/db/mongoose.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose');
-const validate = require('validate')
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { useNewUrlParser: true, useUnifiedTopology: true });
-
